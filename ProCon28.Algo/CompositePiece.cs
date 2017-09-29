653986dacd9ce27970297a1a585c6a2b68aca6f0
cosine1016 <tokizawa1016@gmail.com>
2017-09-29 18:47:15 +0900

    added : CompositePiece  updata : algorithm

---
+++ ProCon28.Algo/CompositePiece.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProCon28.Linker;
+
+namespace ProCon28.Algo
+{
+    class CompositePiece : Piece
+    {
+        public CompositePiece( List<Piece> S) : base()
+        {
+            Source = S;
+        }
+        public List<Piece> Source { get; set; }
+    }
+}
